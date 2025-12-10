c9b4b16b39043cc276fc19563770dbc50526f007
Sonal Singh <sonal1201kumar@gmail.com>
2025-12-10 12:08:26 +0530

    chaining and multiStructOutput

---
+++ LangChain_Ts/src/multi_struc_output+chaining.ts
@@ -0,0 +1,73 @@
+import "dotenv/config";
+import { z } from "zod"
+
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai"
+import { RunnableSequence } from "@langchain/core/runnables"
+
+//schema
+
+const reviewExtract = z.object({
+    rating: z.number(),
+    sentiment: z.enum(["positive", "negative"]),
+    pros: z.array(z.string()),
+    cons: z.array(z.string())
+})
+
+const reviveSummary = z.object({
+    summary: z.string()
+})
+
+const reviewRecommendation = z.object({
+    recommendation: z.string(),
+})
+
+
+const extractModel = new ChatGoogleGenerativeAI({
+    model: "gemini-2.5-flash",
+    temperature: 0
+}).withStructuredOutput(reviewExtract)
+
+const summaryModel = new ChatGoogleGenerativeAI({
+    model: "gemini-2.5-flash",
+    temperature: 0,
+}).withStructuredOutput(reviveSummary)
+
+const recommendationModel = new ChatGoogleGenerativeAI({
+    model: "gemini-2.5-flash",
+    temperature: 0,
+}).withStructuredOutput(reviewRecommendation)
+
+//chaining 
+
+// const chain = RunnableSequence.from([
+//     async (input: { review: string }) => {
+//         return extractModel.invoke(
+//             `Extract rating, sentiment, pros, and cons:\n${input.review}`
+//         );
+//     },
+//     async (extracted) => {
+//         return summaryModel.invoke(
+//             `Summarize this structured review:\n${JSON.stringify(extracted)}`
+//         );
+//     },
+//     async (summary) => {
+//         return recommendationModel.invoke(
+//             `Give a recommendation:\n${JSON.stringify(summary)}`
+//         );
+//     },
+// ]);
+
+const reviewText = `
+Amazing phone! Super fast performance and long battery life,
+but the camera quality is weak, display is not good in this price range.
+`;
+
+const result = await extractModel.invoke(
+    `Extract rating, sentiment, pros, and cons:\n${reviewText}`
+);
+
+
+// const result = await chain.invoke({ review: reviewText });
+
+console.log("\nFINAL OUTPUT:");
+console.log(result);
